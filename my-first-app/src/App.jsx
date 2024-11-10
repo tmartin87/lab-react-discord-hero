bd72f52db2c7ffc75f4b67ecb74494cfabf07884
Tomás Martín <tmartindsgn@gmail.com>
2024-11-10 20:31:00 +0100

    lab done

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <nav className='Navbar'>
+    <img id='Logo_discord' src='.\src\assets\discord-logo-white.png'></img>
+    <img id='menu-icon' src='.\src\assets\menu-icon.png'></img>
+    </nav>
+    <div className="Hero">
+    <h1>IMAGINE A PLACE...</h1>
+    <p>...where you can belong to a school club, a gaming group, or a worldwide art community.Where just you and a handful of friends can spend time togother. A place that makes it easy to talk every day and hang out more often. </p>
+    <button className='button1'>Download for Mac</button>
+    <button className='button2'>Open Discord in your browser</button>
+
+    </div>
+    
+
+
+    </div>
+  );
+}
+
+export default App;
